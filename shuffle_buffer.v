7401d8961e3adc9d1b89cda67fb7f8a7ff5bf5f4
Chiradip Biswas <127591834+Chiradipb02@users.noreply.github.com>
2025-06-09 21:53:29 +0530

    Update shuffle_buffer.v

--- shuffle_buffer.v
+++ shuffle_buffer.v
@@ -21,7 +21,7 @@
 
 
 module shuffle_buffer(//a set of 9 buffers [for 1 kernel (1ch)]
-    clk,data_in,row_add,mode,data_out,reset,op_enable
+    clk,enable,data_in,row_add,mode,data_out,reset,op_enable
     );
     parameter kernel_size=3;
     parameter data_width=8;
@@ -34,31 +34,43 @@ module shuffle_buffer(//a set of 9 buffers [for 1 kernel (1ch)]
     integer i;
     reg shuffle_done;
     reg [pipo_width-1:0] buffer [kernel_size-1:0];
+    input wire enable;
+    
+    
     
     //mode=1 reorder then store
-    always @(posedge mode) begin
+    always @(posedge clk) begin//the mode is updated at posedge of clk only
+        if(enable==1 && mode==1)begin
         for(i=0;i<kernel_size-1;i=i+1)begin
             buffer[i]=buffer[i+1];//blocking should be done these transfer are 1 after another
         end
         shuffle_done=1;
+        end
     end
     
     //the databus brings new 24bit data at each rising edge of the clock [acc to other modules]
     //at the same time the control system will make the mode=0/1. so to avoid clk rising edge vs mode conflict
     //the data is loaded at negative edge of clk
     always @(negedge clk) begin 
-        if(mode==0) begin//normal loading
-            buffer[row_add]<=data_in;
-        end   
-        if(mode==1 && shuffle_done==1)begin 
-            buffer[kernel_size-1]=data_in;//after shuffle is done load in the last row
-            shuffle_done=0;
-        end 
+        if(enable==1)begin
+            if (mode == 0) begin // normal loading
+                buffer[row_add] <= data_in;
+            end   
+             
+            else if (mode == 1 && shuffle_done == 1) begin 
+                buffer[kernel_size-1] <= data_in;  // ? NON-BLOCKING ASSIGNMENT
+                shuffle_done <= 0;                 // ? NON-BLOCKING ASSIGNMENT
+            end 
+       end
     end
+
     
-    always @(posedge op_enable)begin
-        for(i=0;i<kernel_size;i=i+1)begin
-            data_out[(pipo_width*kernel_size-1)-(i*pipo_width)-:pipo_width]<=buffer[i];
-        end
+    always @(*) begin//keep the output on, otherwise problem while loading to core
+    //the core_load_data handles it [whenever the buffer completes, core loads the data]
+        
+            for(i=0;i<kernel_size;i=i+1)begin
+                data_out[(pipo_width*kernel_size-1)-(i*pipo_width)-:pipo_width]<=buffer[i];
+            end
+        
     end
 endmodule
