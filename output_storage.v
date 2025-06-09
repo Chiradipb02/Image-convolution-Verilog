fe3321a2ef260b8f21a3b82f6d7566559177d109
Chiradip Biswas <127591834+Chiradipb02@users.noreply.github.com>
2025-06-09 21:53:58 +0530

    Update output_storage.v

--- output_storage.v
+++ output_storage.v
@@ -19,41 +19,41 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
 module output_storage(
-    ready_signal,core_out,ip_row,ip_col,r_w
+    ready_signal,core_out,ip_row,ip_col
     );
     parameter ch_num=16;
     parameter op_data_width=16;
     parameter kernel_size=3;
-    
+    parameter op_ft_map_row=16-3+1;
     parameter op_buff_ROW=(16-kernel_size+1)*op_data_width;//same for col
     //row_size=col_size=14x16
-    input wire ready_signal,r_w;
+    input wire ready_signal;
     input wire [ch_num*op_data_width-1:0] core_out;
-    input wire [7:0] ip_row,ip_col;
+    input wire [7:0] ip_row;
+    input wire [7:0] ip_col;
     
     
-    reg [op_buff_ROW-1:0] op_mem [0:op_buff_ROW-1];//bits along the col in single row
+    reg [op_buff_ROW-1:0] op_mem [op_buff_ROW-1:0];//bits along the col in single row
     reg op_ready;
     initial begin 
         op_ready=0;
     end
     integer ch_ind;
     
-    always @(posedge r_w)begin
-        //if goes out of bounds, does not get the clocking
-        if(ready_signal && ip_col<op_buff_ROW)begin
-           //top->0th channel image
-           //core_out-> left most->0th channel
+    //only problem: for 1st position in row, it is not loading the pixel value [reason unknown till now]
+    always @(posedge ready_signal) begin
+        if (ip_row < op_buff_ROW && ip_col < op_buff_ROW) begin
             for(ch_ind=0;ch_ind<ch_num;ch_ind=ch_ind+1)begin
-                op_mem[ip_row+ch_num*ch_ind][op_buff_ROW-1-ip_col*op_data_width -:op_data_width]<=core_out[ch_num*op_data_width-1-ch_ind*op_data_width -:op_data_width];
-            end
+                op_ready <= 1;//done for debugging purpose
+                op_mem[ip_row+op_ft_map_row*ch_ind][op_buff_ROW-1-ip_col*op_data_width -:op_data_width]<=core_out[ch_num*op_data_width-1-ch_ind*op_data_width -:op_data_width];
+                end
+            
         end
     end
     
     always @(*) begin 
-        op_ready=0;
-        if(ip_col>=op_buff_ROW) op_ready=1;
+        //op_ready=0;
+        if(ip_col>=op_buff_ROW) op_ready=2;
     end
 endmodule
